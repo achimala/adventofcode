640226d1d8502568b263f99b6ec5f883ab44cb79
Anshu Chimala <achimala@gmail.com>
2015-12-15 13:04:12 +0530

    Part 2 of problem 5.

--- 5.cc
+++ 5.cc
@@ -27,16 +27,44 @@ bool isNice(const std::string& string) {
     return numberOfVowels >= 3 && hasDoubleLetter;
 }
 
+bool isNice2(const std::string& string) {
+    bool foundRepeatingPair = false;
+    bool foundSandwich = false;
+    for (int i = 0; i < string.length() - 2; ++i) {
+        auto pair = string.substr(i, 2);
+        if (string.find(pair, i + 2) != std::string::npos) {
+            foundRepeatingPair = true;
+        }
+
+        auto triplet = string.substr(i, 3);
+        if (triplet[0] == triplet[2]) {
+            foundSandwich = true;
+        }
+
+        if (foundRepeatingPair && foundSandwich) {
+            return true;
+        }
+    }
+
+    return foundRepeatingPair && foundSandwich;
+}
+
 int main() {
     std::string line;
     int count = 0;
+    int count2 = 0;
     while (std::getline(std::cin, line)) {
         if (isNice(line)) {
             ++count;
         }
+
+        if (isNice2(line)) {
+            ++count2;
+        }
     }
 
     std::cout << count << std::endl;
+    std::cout << count2 << std::endl;
     return 0;
 }
 
