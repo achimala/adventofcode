82a955082f23af78acfe134b086511814dd42b54
Anshu Chimala <achimala@gmail.com>
2015-12-15 12:29:30 +0530

    Part 2 of problem 4.

--- 4.cc
+++ 4.cc
@@ -8,17 +8,28 @@ int main(int argc, char** argv) {
     }
 
     std::string secretKey(argv[1]);
+    bool found5 = false;
     for (int i = 0;; ++i) {
         auto string = md5(secretKey + std::to_string(i));
-        bool works = true;
+        bool worksFor5 = true;
+        bool worksFor6 = false;
         for (int j = 0; j < 5; ++j) {
             if (string[j] != '0') {
-                works = false;
+                worksFor5 = false;
                 break;
             }
         }
 
-        if (works) {
+        if (worksFor5 && string[5] == '0') {
+            worksFor6 = true;
+        }
+
+        if (worksFor5 && !found5) {
+            std::cout << i << std::endl;
+            found5 = true;
+        }
+
+        if (worksFor6) {
             std::cout << i << std::endl;
             return 0;
         }
